f6289d7c7cedafa361464b0f06effec3b3af2a23
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2024-07-09 12:26:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express"
 import cors from "cors"
 import morgan from "morgan"
-import swaggerUi from "swagger-ui-express"
+//import swaggerUi from "swagger-ui-express"
 
 import errorHandler from "./middlewares/errorHandler.js"
 import router from "./routes/index.js"
-import swaggerDocument from "./configs/swagger_output.json" assert { type: "json" }
+//import swaggerDocument from "./configs/swagger_output.json" assert { type: "json" }
 
 const app = express()
 
@@ -13,7 +13,7 @@ app.use(express.json({ limit: "100mb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({ origin: "*" }))
 app.use(morgan("dev"))
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to my API" })
